c3fb71f8f3d7a36f05e8f09234a2df5fad07403b
Benjamín_Aage <benjamin18@ru.is>
2018-09-04 14:08:33 +0000

    Algorithm written for maax_int

---
+++ max_int.py
@@ -0,0 +1,10 @@
+# Assignment 5: Algorithms and git
+
+1. Fá notanda til að slá inn tölur (integer).
+2. Láta kerfið greina það hvort um jákvæða eða neikvæða tölu er um að ræða (sem sleginn er inn af notanda).
+3. Flokka jákvæðar tölur saman í lista.
+4. Stoppa própram þegar slegið er inn neikvæða tölu.
+5. Leita að hæstu jákvæða tölugildið sem slegið hefur verið inn.
+6. Prenta út hæsta jákvæða töluna sem slegið var inn af notanda.:x
+
+

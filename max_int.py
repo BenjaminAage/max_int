a71e930aa9ed5bb0ab339e75eac00d2b9bba5df1
Benjamín_Aage <benjamin18@ru.is>
2018-09-04 19:18:10 +0000

    Algorithm code

--- max_int.py
+++ max_int.py
@@ -7,3 +7,19 @@
 # 5. Leita að hæstu jákvæða tölugildið sem slegið hefur verið inn.
 # 6. Prenta út hæsta jákvæða töluna sem slegið var inn af notanda.:x
 
+# Design an algorithm that finds the maximum positive integer input by a user.  The user repeatedly inputs numbers until a negative value is entered.
+
+number_int = int(input("Input a number: "))
+# Búa til lista sem heldur útanum allar jákvæðar tölur sem slegnar eru inn.
+max_int = []
+
+# Check if the number typed is a positive number
+while number_int >= 0:
+  number_int = int(input("Input a number: "))
+  # Add the typed number to the list of positive numbers
+  max_int.extend([number_int])
+
+  # If the number is negative, stop the program
+  if number_int < 0:
+    print("The maximum is",max(max_int))
+
--- sequence.py
+++ sequence.py
@@ -4,11 +4,33 @@
 # Design an algorithm that generates the first n numbers in the following sequence; 1,2,3,6,11,20,37, ___, ___, ___,.....
 
 # 1. Fá notanda til að skrifa inn lengd raðar af tölum (e. sequence).
-
 # 2. Láta kerfi skrifa út muninn milli tveggja talna.
 #       Munur milli 0 og 1, er 1 + munurinn milli 1 og 2.
 #       Dæmi: 3+((1–0)+(2–1)+(3–2)) = 6 +((2–1)+(3–2)+(6–3)) = 11....
-
 # 3. Prenta út niðurstöður á númerum, miðað við lengd raðar sem notandi stimplar inn.
+
+
+# Design an algorithm that generates the first n numbers in the following sequence:; 1, 2, 3, 6, 11, 20, 37, ___, ___, ___, …
+
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+num0 = 0
+num1 = 1
+num2 = 2
+sum = num0 + num1 + num2
+counter = 0
+
+if n <= 0:
+  print("Not possible")
+elif n == 1:
+  print(n)
+elif n >= 2:
+  while counter < n:
+    num0 = num1
+    num1 = num2
+    num2 = sum
+    sum = num0 + num1 + num2
+    print(num0)
+    counter += 1
 
 

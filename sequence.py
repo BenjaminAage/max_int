ec9da0fb2262931c82ab47c5fad3df8dfb3156ba
Benjamín_Aage <benjamin18@ru.is>
2018-09-04 14:51:10 +0000

    Algorithm written for sequence

---
+++ sequence.py
@@ -0,0 +1,7 @@
+# Assignment 5: Algorithms and git
+# Algorithm for "Assigment 2" - Class work 4.Sept 2018:
+
+# Design an algorithm that generates the first n numbers in the following sequence; 1,2,3,6,11,20,37, ___, ___, ___,.....
+
+# 1.
+

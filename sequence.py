d1e9715e59f23ea0cfe71ba24a47eb9d5ff24036
Benjamín_Aage <benjamin18@ru.is>
2018-09-04 15:03:38 +0000

    Algorithm written for sequence

--- sequence.py
+++ sequence.py
@@ -3,5 +3,12 @@
 
 # Design an algorithm that generates the first n numbers in the following sequence; 1,2,3,6,11,20,37, ___, ___, ___,.....
 
-# 1.
+# 1. Fá notanda til að skrifa inn lengd raðar af tölum (e. sequence).
+
+# 2. Láta kerfi skrifa út muninn milli tveggja talna.
+#       Munur milli 0 og 1, er 1 + munurinn milli 1 og 2.
+#       Dæmi: 3+((1–0)+(2–1)+(3–2)) = 6 +((2–1)+(3–2)+(6–3)) = 11....
+
+# 3. Prenta út niðurstöður á númerum, miðað við lengd raðar sem notandi stimplar inn.
+
 
